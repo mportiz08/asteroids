40011c1fd86d3c6ed239c8857ad661d8e917d49a
Marcus Ortiz <mportiz08@gmail.com>
2012-03-12 23:08:11 -0700

    ship movement

--- ship.lua
+++ ship.lua
@@ -5,10 +5,14 @@ local ship = {}
 
 ship.width              = 10
 ship.height             = 20
-ship.torque             = 50
+ship.speed              = 60
+ship.torque             = 100
 ship.mass               = 15
-ship.inertia            = 15
+ship.inertia            = 30
 ship.maxAngularVelocity = 5
+ship.maxLinearVelocity  = 50
+ship.linearDamping      = 1
+ship.angularDamping     = 1
 ship.color              = {r = 255, g = 255, b = 255}
 
 function ship.load()
@@ -27,22 +31,24 @@ function ship.load()
       y = ship.height / 2
     }
   }
-  ship.body = love.physics.newBody(space.world, center.x, center.y, 15, 15)
+  ship.body = love.physics.newBody(space.world, center.x, center.y, ship.mass, ship.inertia)
   ship.shape = love.physics.newPolygonShape(ship.body, offsets.top.x, offsets.top.y,
                                             offsets.bottomRight.x, offsets.bottomRight.y,
                                             offsets.bottomLeft.x, offsets.bottomLeft.y)
+  ship.body:setLinearDamping(ship.linearDamping)
+  ship.body:setAngularDamping(ship.angularDamping)
 end
 
 function ship.update(dt)
+  angle = ship.body:getAngle()
   if love.keyboard.isDown('left') then
-    if ship.body:getAngularVelocity() > 0 - ship.maxAngularVelocity then
-      ship.body:applyTorque(0 - ship.torque)
-    end
+    ship.body:applyTorque(0 - ship.torque)
   end
   if love.keyboard.isDown('right') then
-    if ship.body:getAngularVelocity() < ship.maxAngularVelocity then
-      ship.body:applyTorque(ship.torque)
-    end
+    ship.body:applyTorque(ship.torque)
+  end
+  if love.keyboard.isDown('up') then
+    ship.body:applyForce(ship.speed * math.sin(angle), 0 - (ship.speed * math.cos(angle)))
   end
 end
 
