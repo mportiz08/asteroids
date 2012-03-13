f8a016f850f77faf27284bb3a73d567761eb35e8
Marcus Ortiz <mportiz08@gmail.com>
2012-03-12 22:45:42 -0700

    rotating ship

--- ship.lua
+++ ship.lua
@@ -3,9 +3,13 @@ local space    = require('space')
 
 local ship = {}
 
-ship.width  = 10
-ship.height = 20
-ship.color  = {r = 255, g = 255, b = 255}
+ship.width              = 10
+ship.height             = 20
+ship.torque             = 50
+ship.mass               = 15
+ship.inertia            = 15
+ship.maxAngularVelocity = 5
+ship.color              = {r = 255, g = 255, b = 255}
 
 function ship.load()
   center = {x = settings.width / 2, y = settings.height / 2}
@@ -23,14 +27,23 @@ function ship.load()
       y = ship.height / 2
     }
   }
-  ship.body = love.physics.newBody(space.world, center.x, center.y, 15, 0)
+  ship.body = love.physics.newBody(space.world, center.x, center.y, 15, 15)
   ship.shape = love.physics.newPolygonShape(ship.body, offsets.top.x, offsets.top.y,
                                             offsets.bottomRight.x, offsets.bottomRight.y,
                                             offsets.bottomLeft.x, offsets.bottomLeft.y)
 end
 
-function ship.update()
-  -- TODO
+function ship.update(dt)
+  if love.keyboard.isDown('left') then
+    if ship.body:getAngularVelocity() > 0 - ship.maxAngularVelocity then
+      ship.body:applyTorque(0 - ship.torque)
+    end
+  end
+  if love.keyboard.isDown('right') then
+    if ship.body:getAngularVelocity() < ship.maxAngularVelocity then
+      ship.body:applyTorque(ship.torque)
+    end
+  end
 end
 
 function ship.draw()
